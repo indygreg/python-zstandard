28a7287e7609d4d72bae30627b6ce3528b3ab30c
Gregory Szorc <gregory.szorc@gmail.com>
2017-01-01 19:35:14 -0800

    Always execute vcvarsall.bat in AppVeyor
    Previously, some Python versions were relying on functionality in
    modern distutils to find the appropriate toolchain. This kinda/sorta
    worked, but was inconsistent and turned out to be a bit fragile.
    For example, it was executing a script in the SDK directory, which
    hard coded certain compiler versions.
    
    In this commit, we change our script that sets up the environment
    to always call vcvarsall.bat to set up the environment. We also
    tell distutils to always use the settings exported by the
    environment instead of guessing them. The execution environment
    is consistent between Python versions and no longer relies on
    smarts in distutils.

--- ci/run-with-env.cmd
+++ ci/run-with-env.cmd
@@ -1,14 +1,3 @@
-:: To build extensions for 64 bit Python 3, we need to configure environment
-:: variables to use the MSVC 2010 C++ compilers from GRMSDKX_EN_DVD.iso of:
-:: MS Windows SDK for Windows 7 and .NET Framework 4 (SDK v7.1)
-::
-:: To build extensions for 64 bit Python 2, we need to configure environment
-:: variables to use the MSVC 2008 C++ compilers from GRMSDKX_EN_DVD.iso of:
-:: MS Windows SDK for Windows 7 and .NET Framework 3.5 (SDK v7.0)
-::
-:: 32 bit builds, and 64-bit builds for 3.5 and beyond, do not require specific
-:: environment configurations.
-::
 :: Note: this script needs to be run with the /E:ON and /V:ON flags for the
 :: cmd interpreter, at least for (SDK v7.0)
 ::
@@ -18,17 +7,6 @@
 ::
 :: Author: Olivier Grisel
 :: License: CC0 1.0 Universal: http://creativecommons.org/publicdomain/zero/1.0/
-::
-:: Notes about batch files for Python people:
-::
-:: Quotes in values are literally part of the values:
-::      SET FOO="bar"
-:: FOO is now five characters long: " b a r "
-:: If you don't want quotes, don't include them on the right-hand side.
-::
-:: The CALL lines at the end of this file look redundant, but if you move them
-:: outside of the IF clauses, they do not run properly in the SET_SDK_64==Y
-:: case, I don't know why.
 @ECHO OFF
 
 SET COMMAND_TO_RUN=%*
@@ -44,45 +22,57 @@ IF "%PYTHON_VERSION:~3,1%" == "." (
     SET MINOR_PYTHON_VERSION=%PYTHON_VERSION:~2,2%
 )
 
-:: Based on the Python version, determine what SDK version to use, and whether
-:: to set the SDK for 64-bit.
+:: Modern versions of distutils attempt to guess the toolchain to use based
+:: on ``sys.version`` and from inspecting the system (notably the registry).
+:: This is somewhat fragile in AppVeyor for some reason. So, we instead set
+:: ``DISTUTILS_USE_SDK`` and ``MSSdk`` along with some other environment
+:: variables (some set by ``vcvarsall.bat``) to force distutils to use a
+:: specific toolchain and SDK.
+
 IF %MAJOR_PYTHON_VERSION% == 2 (
     SET WINDOWS_SDK_VERSION="v7.0"
-    SET SET_SDK_64=Y
+    SET TOOLS_VERSION="9.0"
+    SET VCVARS=vcvars64.bat
 ) ELSE (
     IF %MAJOR_PYTHON_VERSION% == 3 (
         SET WINDOWS_SDK_VERSION="v7.1"
-        IF %MINOR_PYTHON_VERSION% LEQ 4 (
-            SET SET_SDK_64=Y
+        IF %MINOR_PYTHON_VERSION% LEQ 2 (
+            ECHO Unsupported Python version: "%MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION%"
+            EXIT 1
         ) ELSE (
-            SET SET_SDK_64=N
-            IF EXIST "%WIN_WDK%" (
-                :: See: https://connect.microsoft.com/VisualStudio/feedback/details/1610302/
-                REN "%WIN_WDK%" 0wdf
+            IF %MINOR_PYTHON_VERSION% LEQ 4 (
+                SET WINDOWS_SDK_VERSION="v7.1"
+                SET TOOLS_VERSION="10.0"
+                SET VCVARS=amd64\vcvars64.bat
+            ) ELSE (
+                IF %MINOR_PYTHON_VERSION% LEQ 6 (
+                    SET WINDOWS_SDK_VERSION="v7.1"
+                    SET TOOLS_VERSION="14.0"
+                    SET VCVARS=amd64\vcvars64.bat
+                ) ELSE (
+                    ECHO Unsupported Python version: "%MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION%"
+                    EXIT 1
+                )
             )
         )
-    ) ELSE (
-        ECHO Unsupported Python version: "%MAJOR_PYTHON_VERSION%"
-        EXIT 1
+
+        IF EXIST "%WIN_WDK%" (
+            :: See: https://connect.microsoft.com/VisualStudio/feedback/details/1610302/
+            REN "%WIN_WDK%" 0wdf
+        )
     )
 )
 
-IF %PYTHON_ARCH% == 64 (
-    IF %SET_SDK_64% == Y (
-        ECHO Configuring Windows SDK %WINDOWS_SDK_VERSION% for Python %MAJOR_PYTHON_VERSION% on a 64 bit architecture
-        SET DISTUTILS_USE_SDK=1
-        SET MSSdk=1
-        "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
-        "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /release
-        ECHO Executing: %COMMAND_TO_RUN%
-        call %COMMAND_TO_RUN% || EXIT 1
-    ) ELSE (
-        ECHO Using default MSVC build environment for 64 bit architecture
-        ECHO Executing: %COMMAND_TO_RUN%
-        call %COMMAND_TO_RUN% || EXIT 1
-    )
-) ELSE (
-    ECHO Using default MSVC build environment for 32 bit architecture
+SET DISTUTILS_USE_SDK=1
+SET MSSdk=1
+"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
+
+if %PYTHON_ARCH% == 64 (
+    CALL "C:\Program Files (x86)\Microsoft Visual Studio %TOOLS_VERSION%\VC\bin\%VCVARS%" || EXIT 1
     ECHO Executing: %COMMAND_TO_RUN%
-    call %COMMAND_TO_RUN% || EXIT 1
+    CALL %COMMAND_TO_RUN% || EXIT 1
+) ELSE (
+    CALL "C:\Program Files (x86)\Microsoft Visual Studio %TOOLS_VERSION%\VC\vcvarsall.bat" x86 || EXIT 1
+    ECHO Executing: %COMMAND_TO_RUN%
+    CALL %COMMAND_TO_RUN% || EXIT 1
 )
