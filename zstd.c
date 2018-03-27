59fb3c89704b7621021da152ca93ce477a31b8f9
Gregory Szorc <gregory.szorc@gmail.com>
2018-03-26 17:42:17 -0700

    zstd: update zstandard version number
    And now the extension can load when linked against 1.3.4.

--- zstd.c
+++ zstd.c
@@ -109,7 +109,7 @@ void zstd_module_init(PyObject* m) {
 	   We detect this mismatch here and refuse to load the module if this
 	   scenario is detected.
 	*/
-	if (ZSTD_VERSION_NUMBER != 10303 || ZSTD_versionNumber() != 10303) {
+	if (ZSTD_VERSION_NUMBER != 10304 || ZSTD_versionNumber() != 10304) {
 		PyErr_SetString(PyExc_ImportError, "zstd C API mismatch; Python bindings not compiled against expected zstd version");
 		return;
 	}
