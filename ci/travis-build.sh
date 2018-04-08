3b434694daa07b28c6b1b95430a05a2a89ff48cc
Gregory Szorc <gregory.szorc@gmail.com>
2018-04-07 18:48:07 -0700

    ci: add explicit tests of manylinux wheels
    We now have test coverage for wheels built locally and the manylinux
    wheels.
    
    As part of this, I discovered that S3 artifacts weren't publicly
    readable. So we fix that.

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -8,6 +8,8 @@
 set -ex
 
 MANYLINUX_PYPATHS="cp27-cp27m cp27-cp27mu cp34-cp34m cp35-cp35m cp36-cp36m"
+CURRENT_VERSION=zstandard-0.9.0.dev0
+MANYLINUX_URL=https://s3-us-west-2.amazonaws.com/python-zstandard/travis/${TRAVIS_BUILD_NUMBER}/manylinuxwheels/dist
 
 function make_manylinux_wheels {
     image=$1
@@ -27,6 +29,23 @@ function make_manylinux_wheels {
     done
 }
 
+function resolve_wheel {
+    if [ "${TRAVIS_PYTHON_VERSION}" = "2.7" ]; then
+        pypart=cp27-cp27mu
+    elif [ "${TRAVIS_PYTHON_VERSION}" = "3.4" ]; then
+        pypart=cp34-cp34m
+    elif [ "${TRAVIS_PYTHON_VERSION}" = "3.5" ]; then
+        pypart=cp35-cp35m
+    elif [ "${TRAVIS_PYTHON_VERSION}" = "3.6" ]; then
+        pypart=cp36-cp36m
+    else
+        echo "Unsure what version of Python we're running: ${TRAVIS_PYTHON_VERSION}"
+        exit 1
+    fi
+
+    echo ${CURRENT_VERSION}-${pypart}-manylinux1_x86_64.whl
+}
+
 if [ "${BUILDMODE}" = "conda" ]; then
     conda build ci/conda
     mkdir -p dist
@@ -35,13 +54,27 @@ if [ "${BUILDMODE}" = "conda" ]; then
 elif [ "${BUILDMODE}" = "manylinuxwheels" ]; then
     make_manylinux_wheels quay.io/pypa/manylinux1_x86_64
     make_manylinux_wheels quay.io/pypa/manylinux1_i686 linux32
+
 elif [ "${BUILDMODE}" = "cibuildwheel" ]; then
     cibuildwheel --output-dir dist
+
 elif [ "${BUILDMODE}" = "tox" ]; then
     tox
+
+elif [ "${BUILDMODE}" = "manylinuxtest" ]; then
+    mkdir /tmp/wheels
+
+    wheel=$(resolve_wheel)
+    curl ${MANYLINUX_URL}/${wheel} > /tmp/wheels/${wheel}
+
+    pip install /tmp/wheels/${wheel}
+
+    nosetests -v
+
 elif [ -n "${BUILDMODE}" ]; then
     echo "unknown BUILDMODE: ${BUILDMODE}"
     exit 1
+
 else
     echo "BUILDMODE must be defined"
     exit 1
