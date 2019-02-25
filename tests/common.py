735771961bc04f8f7de9372297921826a814fd12
Gregory Szorc <gregory.szorc@gmail.com>
2019-02-25 15:55:37 -0800

    tests: tweak hypothesis CI settings
    This should make things a bit faster.

--- tests/common.py
+++ tests/common.py
@@ -175,7 +175,7 @@ if hypothesis:
     default_settings = hypothesis.settings(deadline=10000)
     hypothesis.settings.register_profile('default', default_settings)
 
-    ci_settings = hypothesis.settings(deadline=10000, max_examples=2500)
+    ci_settings = hypothesis.settings(deadline=20000, max_examples=1000)
     hypothesis.settings.register_profile('ci', ci_settings)
 
     expensive_settings = hypothesis.settings(deadline=None, max_examples=10000)
