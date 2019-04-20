aa30fbe200d18664e168efa49ca25de37b4dd5fe
Gregory Szorc <gregory.szorc@gmail.com>
2019-04-20 12:50:58 -0700

    global: bump version to 0.12.0.dev0
    The march continues.

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -8,7 +8,7 @@
 set -ex
 
 MANYLINUX_PYPATHS="cp27-cp27m cp27-cp27mu cp34-cp34m cp35-cp35m cp36-cp36m cp37-cp37m"
-CURRENT_VERSION=zstandard-0.11.0
+CURRENT_VERSION=zstandard-0.12.0.dev0
 MANYLINUX_URL=https://s3-us-west-2.amazonaws.com/python-zstandard/travis/${TRAVIS_BUILD_NUMBER}/manylinuxwheels/dist
 
 function make_manylinux_wheels {
