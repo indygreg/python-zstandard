43929003dc3dfbefa76eb8d1b9d3a3363a1d096a
Gregory Szorc <gregory.szorc@gmail.com>
2020-06-13 13:20:46 -0700

    rust: add Python module initializer boilerplate
    It doesn't do much except define the docstring. But it is a start.

--- rust-ext/src/lib.rs
+++ rust-ext/src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// Copyright (c) 2020-present, Gregory Szorc
+// All rights reserved.
+//
+// This software may be modified and distributed under the terms
+// of the BSD license. See the LICENSE file for details.
+
+use cpython::{py_module_initializer, Python, PyModule, PyResult};
+
+// add bindings to the generated python module
+// N.B: names: "rust2py" must be the name of the `.so` or `.pyd` file
+py_module_initializer!(zstandard_oxidized, |py, m| {
+    init_module(py, m)
+});
+
+fn init_module(py: Python, module: &PyModule) -> PyResult<()> {
+    module.add(py, "__doc__", "Rust backend for zstandard bindings")?;
+
+    Ok(())
 }
