156b212c8f3883eace8b02c96406b432b93493ba
Gregory Szorc <gregory.szorc@gmail.com>
2020-12-05 11:27:05 -0800

    setup: add Python 3.9 to classifiers

--- setup.py
+++ setup.py
@@ -136,6 +136,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="zstandard zstd compression",
     packages=["zstandard"],
