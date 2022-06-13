41efb8abf978b12ecf91110482ca37055d68a80e
Gregory Szorc <gregory.szorc@gmail.com>
2022-06-12 18:13:08 -0700

    rust: remove try_reserve feature
    It is enabled by default as of Rust 1.57.

--- rust-ext/src/lib.rs
+++ rust-ext/src/lib.rs
@@ -4,8 +4,6 @@
 // This software may be modified and distributed under the terms
 // of the BSD license. See the LICENSE file for details.
 
-#![feature(try_reserve)]
-
 use pyo3::{prelude::*, types::PySet};
 
 mod buffers;
