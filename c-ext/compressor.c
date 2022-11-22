6bc6ff7791be321ce6727cb411698c0a21787f63
Mike Hommey <mh@glandium.org>
2022-11-22 19:05:11 +0900

    Use PyType_Spec to define types instead of PyTypeObject: ZstdCompressor

--- c-ext/compressor.c
+++ c-ext/compressor.c
@@ -1528,52 +1528,30 @@ static PyMethodDef ZstdCompressor_methods[] = {
      METH_NOARGS, NULL},
     {NULL, NULL}};
 
-PyTypeObject ZstdCompressorType = {
-    PyVarObject_HEAD_INIT(NULL, 0) "zstd.ZstdCompressor", /* tp_name */
-    sizeof(ZstdCompressor),                               /* tp_basicsize */
-    0,                                                    /* tp_itemsize */
-    (destructor)ZstdCompressor_dealloc,                   /* tp_dealloc */
-    0,                                                    /* tp_print */
-    0,                                                    /* tp_getattr */
-    0,                                                    /* tp_setattr */
-    0,                                                    /* tp_compare */
-    0,                                                    /* tp_repr */
-    0,                                                    /* tp_as_number */
-    0,                                                    /* tp_as_sequence */
-    0,                                                    /* tp_as_mapping */
-    0,                                                    /* tp_hash */
-    0,                                                    /* tp_call */
-    0,                                                    /* tp_str */
-    0,                                                    /* tp_getattro */
-    0,                                                    /* tp_setattro */
-    0,                                                    /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,             /* tp_flags */
-    0,                                                    /* tp_doc */
-    0,                                                    /* tp_traverse */
-    0,                                                    /* tp_clear */
-    0,                                                    /* tp_richcompare */
-    0,                             /* tp_weaklistoffset */
-    0,                             /* tp_iter */
-    0,                             /* tp_iternext */
-    ZstdCompressor_methods,        /* tp_methods */
-    0,                             /* tp_members */
-    0,                             /* tp_getset */
-    0,                             /* tp_base */
-    0,                             /* tp_dict */
-    0,                             /* tp_descr_get */
-    0,                             /* tp_descr_set */
-    0,                             /* tp_dictoffset */
-    (initproc)ZstdCompressor_init, /* tp_init */
-    0,                             /* tp_alloc */
-    PyType_GenericNew,             /* tp_new */
+PyType_Slot ZstdCompressorSlots[] = {
+    {Py_tp_dealloc, ZstdCompressor_dealloc},
+    {Py_tp_methods, ZstdCompressor_methods},
+    {Py_tp_init, ZstdCompressor_init},
+    {Py_tp_new, PyType_GenericNew},
+    {0, NULL},
 };
 
+PyType_Spec ZstdCompressorSpec = {
+    "zstd.ZstdCompressor",
+    sizeof(ZstdCompressor),
+    0,
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    ZstdCompressorSlots,
+};
+
+PyTypeObject *ZstdCompressorType;
+
 void compressor_module_init(PyObject *mod) {
-    Py_SET_TYPE(&ZstdCompressorType, &PyType_Type);
-    if (PyType_Ready(&ZstdCompressorType) < 0) {
+    ZstdCompressorType = (PyTypeObject *)PyType_FromSpec(&ZstdCompressorSpec);
+    if (PyType_Ready(ZstdCompressorType) < 0) {
         return;
     }
 
-    Py_INCREF((PyObject *)&ZstdCompressorType);
-    PyModule_AddObject(mod, "ZstdCompressor", (PyObject *)&ZstdCompressorType);
+    Py_INCREF((PyObject *)ZstdCompressorType);
+    PyModule_AddObject(mod, "ZstdCompressor", (PyObject *)ZstdCompressorType);
 }
