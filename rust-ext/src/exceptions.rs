02a86c6f6527ea88b607745a8094f4c263675fc9
Gregory Szorc <gregory.szorc@gmail.com>
2024-05-25 15:54:23 -0700

    rust: use py.get_type_bound

--- rust-ext/src/exceptions.rs
+++ rust-ext/src/exceptions.rs
@@ -9,7 +9,7 @@ use pyo3::{create_exception, exceptions::PyException, prelude::*};
 create_exception!(module, ZstdError, PyException);
 
 pub(crate) fn init_module(py: Python, module: &PyModule) -> PyResult<()> {
-    module.add("ZstdError", py.get_type::<ZstdError>())?;
+    module.add("ZstdError", py.get_type_bound::<ZstdError>())?;
 
     Ok(())
 }
