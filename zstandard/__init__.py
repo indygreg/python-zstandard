f6e7693b7e64b36fdf3ba4df89335d97b5ec57c6
Gregory Szorc <gregory.szorc@gmail.com>
2024-11-03 19:18:45 -0800

    zstandard: suppress ruff lints related to star imports

--- zstandard/__init__.py
+++ zstandard/__init__.py
@@ -4,6 +4,8 @@
 # This software may be modified and distributed under the terms
 # of the BSD license. See the LICENSE file for details.
 
+# ruff: noqa: F403, F405
+
 """Python interface to the Zstandard (zstd) compression library."""
 
 from __future__ import absolute_import, unicode_literals
