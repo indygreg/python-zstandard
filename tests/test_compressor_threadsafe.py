da3aec8095ccf3bb0cbd1e625f89b3c830ff4d62
parmeggiani <parmeggiani@spaziodati.eu>
2024-11-27 19:47:43 +0100

    fix test_shared_compressor

--- tests/test_compressor_threadsafe.py
+++ tests/test_compressor_threadsafe.py
@@ -1,5 +1,5 @@
 import unittest
-from threading import Barrier, Thread
+from threading import Barrier, Lock, Thread
 
 import pytest
 
@@ -12,18 +12,28 @@ class TestCompressor_threadsafe(unittest.TestCase):
         num_parallel_threads = 10
         cctx = zstd.ZstdCompressor()
         barrier = Barrier(num_parallel_threads)
+        raised_exceptions = 0
+        raised_exceptions_lock = Lock()
 
         def thread():
+            nonlocal raised_exceptions
+
             barrier.wait()
-            with self.assertRaises(zstd.ZstdError):
+            try:
                 for _ in range(1_000):
                     cctx.compress(b"t" * 1048576)
+            except zstd.ZstdError:
+                with raised_exceptions_lock:
+                    raised_exceptions += 1
 
         threads = [
             Thread(target=thread)
             for _ in range(num_parallel_threads)
         ]
+        # time.sleep(10)
         for t in threads:
             t.start()
         for t in threads:
             t.join()
+
+        assert raised_exceptions == num_parallel_threads - 1
