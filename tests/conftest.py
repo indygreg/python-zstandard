40282f843c770ecd9b61186170a18f58867fd277
Gregory Szorc <gregory.szorc@gmail.com>
2025-08-24 13:12:22 -0700

    tests: auto skip cffi backend on 3.13 free-threaded builds
    cffi doesn't support free-threading until the 2.0 release. And the
    2.0 release only implements it on 3.14+. So refuse to run tests against
    the cffi backend on 3.13 free-threaded builds.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,24 @@
 import os
+import sys
+import sysconfig
+
+import pytest
 
 try:
     import hypothesis  # type: ignore
 except ImportError:
     hypothesis = None  # type: ignore
+
+
+def pytest_configure(config):
+    if (
+        os.environ.get("PYTHON_ZSTANDARD_IMPORT_POLICY", "").strip() == "cffi"
+        and sysconfig.get_config_var("Py_GIL_DISABLED") == 1
+        and sys.version_info[0:2] == (3, 13)
+    ):
+        pytest.exit(
+            "cffi backend not supported on 3.13 free-threaded Python", 0
+        )
 
 
 if hypothesis:
