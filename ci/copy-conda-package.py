e6762e293de5b502d3700d5212331c1e99e3a612
Gregory Szorc <gregory.szorc@gmail.com>
2026-07-05 09:46:11 -0700

    ci: use conda-incubator/setup-miniconda action
    The conda CI jobs are failing. And Windows has been busted for a while. Let's
    just use the official action for setting up Conda to hopefully pave over
    problems.

--- ci/copy-conda-package.py
+++ ci/copy-conda-package.py
@@ -1,19 +1,25 @@
 import os
+import platform
 import shutil
 
-from conda_build.config import Config
+if platform.system() == "Linux":
+    build_dir = "/usr/share/miniconda/conda-bld"
+elif platform.system() == "Windows":
+    build_dir = "C:/Miniconda/conda-bld"
+else:
+    raise Exception("Unsupported platform: %s", platform.system())
 
-build_dir = Config().bldpkgs_dir
 dest_dir = "dist"
 
 if not os.path.exists(dest_dir):
     os.mkdir(dest_dir)
 
 print("scanning %s for packages" % build_dir)
-for p in os.listdir(build_dir):
-    if not p.endswith(".conda"):
-        continue
+for root, _, files in os.walk(build_dir):
+    for f in files:
+        source = os.path.join(root, f)
+        if not source.endswith(".conda"):
+            continue
 
-    source = os.path.join(build_dir, p)
-    print("copying %s" % source)
-    shutil.copyfile(source, os.path.join(dest_dir, p))
+        print("copying %s" % source)
+        shutil.copyfile(source, os.path.join(dest_dir, f))
